a781cfa5ca315bd4243f7a8eaa160da358149563
Alexandra Wasef <alex.wasef@gmail.com>
2022-02-22 00:56:22 +0100

    first commit

---
+++ src/Entity/Lieu.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LieuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LieuRepository::class)]
+class Lieu
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
